c8a60442d23f702204376e2fbf9398fa9599259b
Venkatrao07 <146806210+Venkatrao07@users.noreply.github.com>
2023-10-09 11:16:00 +0530

    Create add.java
    java file

---
+++ add.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Add
+{
+public static void main (String[] args)
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int m=sc.nextInt();
+int x=n+m;
+System.out.println(x);
+}
+}
